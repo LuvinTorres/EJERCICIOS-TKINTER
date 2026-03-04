272f034abf8781c3a3ec5935fd3fadaee9966745
LuvinTorres <luvin706@gmail.com>
2026-03-03 20:16:02 -0500

    modelo_principal.py

--- ParqueaderO/modelo_principal.py
+++ ParqueaderO/modelo_principal.py
@@ -70,20 +70,20 @@ obj_ventana.title("PARQUEADERO")
 obj_ventana.geometry("600x600")
 
 
-titulo_label = ventana.Label(obj_ventana, text="     PARQUEADERO     ")
-titulo_label.config(bg="#2C3E50", font=("Times New Roman", 30), fg="#FFFFFF")
+titulo_label = ventana.Label(obj_ventana, text="                     PARQUEADERO                    ")
+titulo_label.config(bg="#502C2C", font=("Times New Roman", 30), fg="#FFFFFF")
 titulo_label.pack()
 
-seccion1 = ventana.Frame(obj_ventana, bg="#2C3E50")
+seccion1 = ventana.Frame(obj_ventana, bg="#6B0000")
 seccion1.pack()
 
-titulo_seccion1 = ventana.Label(seccion1, text="    Datos de usuario   ")
-titulo_seccion1.config(bg="#2C3E50", font=("Times New Roman", 20), fg="#ECF0F1")
+titulo_seccion1 = ventana.Label(seccion1, text="                     Datos de usuario                     ")
+titulo_seccion1.config(bg="#6B0000", font=("Times New Roman", 20), fg="#ECF0F1")
 titulo_seccion1.pack()
 
 #-----------------------------------------------------------------------
-label_nombre = ventana.Label(seccion1, text="Digite el nombre:" )
-label_nombre.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_nombre = ventana.Label(seccion1, text="                 Digite el nombre:                  " )
+label_nombre.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_nombre.pack()
 
 entry_nombre=ventana.Entry(seccion1)
@@ -91,8 +91,8 @@ entry_nombre.config(bg="black", font=("Helvetica", 13), fg="white")
 entry_nombre.pack()
 #-----------------------------------------------------------------------
 
-label_cedula = ventana.Label(seccion1, text="Numero de cedula:")
-label_cedula.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_cedula = ventana.Label(seccion1, text="                 Numero de cedula:                ")
+label_cedula.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_cedula.pack()
 
 entry_cedula=ventana.Entry(seccion1)
@@ -101,32 +101,32 @@ entry_cedula.pack()
 
 #-----------------------------------------------------------------------
 
-label_tipo_cliente= ventana.Label(seccion1, text="Tipo de cliente:")
-label_tipo_cliente.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_tipo_cliente= ventana.Label(seccion1, text="                     Tipo de cliente:                     ")
+label_tipo_cliente.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_tipo_cliente.pack()
 
 entry_tipo_cliente=ventana.Entry(seccion1)
 entry_tipo_cliente.config(bg="black", font=("Helvetica", 13), fg="white")
 entry_tipo_cliente.pack()
 
-label_placa= ventana.Label(seccion1, text="Placa:")
-label_placa.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_placa= ventana.Label(seccion1, text="                     Placa:                     ")
+label_placa.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_placa.pack()
 
 entry_placa=ventana.Entry(seccion1)
 entry_placa.config(bg="black", font=("Helvetica", 13), fg="white")
 entry_placa.pack()
 
-label_marca= ventana.Label(seccion1, text="Marca:")
-label_marca.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_marca= ventana.Label(seccion1, text="                     Marca:                     ")
+label_marca.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_marca.pack()
 
 entry_marca=ventana.Entry(seccion1)
 entry_marca.config(bg="black", font=("Helvetica", 13), fg="white")
 entry_marca.pack()
 
-label_color= ventana.Label(seccion1, text="Color:")
-label_color.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_color= ventana.Label(seccion1, text="                     Color:                     ")
+label_color.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_color.pack()
 
 entry_color=ventana.Entry(seccion1)
@@ -134,8 +134,8 @@ entry_color.config(bg="black", font=("Helvetica", 13), fg="white")
 entry_color.pack()
 
 #-----------------------------------------------------------------------
-label_hora = ventana.Label(seccion1, text="Hora (entrada o salida):")
-label_hora.config(bg="#2C3E50", font=("Times New Roman", 13), fg="#ECF0F1")
+label_hora = ventana.Label(seccion1, text="                     Hora (entrada o salida):                     ")
+label_hora.config(bg="#6B0000", font=("Times New Roman", 13), fg="#ECF0F1")
 label_hora.pack()
 
 entry_hora = ventana.Entry(seccion1)
@@ -163,6 +163,9 @@ boton_espacio.pack(pady=5)
 boton_acumulado = ventana.Button(seccion1, text="Mostrar Registros", command=mostrar_registros)
 boton_acumulado.config(bg="black", font=("Times New Roman", 8), fg="#F1C40F")
 boton_acumulado.pack(pady=5)
+
+
+obj_ventana.mainloop()
 
 
 obj_ventana.mainloop()
